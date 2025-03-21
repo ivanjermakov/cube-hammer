4a1ecd50f1ae387a3eef7b2d7eaa95bd4bda1988
ivanjermakov <ivanjermakov1@gmail.com>
2025-03-21 17:04:00 +0100

    L and R moves

--- src/lib.rs
+++ src/lib.rs
@@ -57,21 +57,65 @@ impl Cube3by3 {
         }
     }
 
+    #[rustfmt::skip]
     pub fn apply_move(&mut self, m: &Move) {
+        // [fs[0][0], fs[0][1], fs[0][2], fs[0][3], fs[0][4], fs[0][5], fs[0][6], fs[0][7]],
+        // [fs[1][0], fs[1][1], fs[1][2], fs[1][3], fs[1][4], fs[1][5], fs[1][6], fs[1][7]],
+        // [fs[2][0], fs[2][1], fs[2][2], fs[2][3], fs[2][4], fs[2][5], fs[2][6], fs[2][7]],
+        // [fs[3][0], fs[3][1], fs[3][2], fs[3][3], fs[3][4], fs[3][5], fs[3][6], fs[3][7]],
+        // [fs[4][0], fs[4][1], fs[4][2], fs[4][3], fs[4][4], fs[4][5], fs[4][6], fs[4][7]],
+        // [fs[5][0], fs[5][1], fs[5][2], fs[5][3], fs[5][4], fs[5][5], fs[5][6], fs[5][7]],
+
+        let fs = self.faces;
         match m {
-            Move::R => todo!(),
-            Move::Rp => todo!(),
-            Move::L => todo!(),
-            Move::Lp => todo!(),
+            Move::R => {
+                self.faces = [
+                    [fs[0][0], fs[0][1], fs[3][2], fs[3][3], fs[3][4], fs[0][5], fs[0][6], fs[0][7]],
+                    [fs[1][0], fs[1][1], fs[5][6], fs[5][7], fs[5][0], fs[1][5], fs[1][6], fs[1][7]],
+                    [fs[2][0], fs[2][1], fs[2][2], fs[2][3], fs[2][4], fs[2][5], fs[2][6], fs[2][7]],
+                    [fs[3][0], fs[3][1], fs[1][2], fs[1][3], fs[1][4], fs[3][5], fs[3][6], fs[3][7]],
+                    [fs[4][6], fs[4][7], fs[4][0], fs[4][1], fs[4][2], fs[4][3], fs[4][4], fs[4][5]],
+                    [fs[0][4], fs[5][1], fs[5][2], fs[5][3], fs[5][4], fs[5][5], fs[0][3], fs[0][2]],
+                ]
+            }
+            Move::Rp => for _ in 0..3 {
+                self.apply_move(&Move::R);
+            }
+            Move::R2 => for _ in 0..2 {
+                self.apply_move(&Move::R);
+            },
+            Move::L => {
+                self.faces = [
+                    [fs[5][4], fs[0][1], fs[0][2], fs[0][3], fs[0][4], fs[0][5], fs[5][3], fs[5][2]],
+                    [fs[3][0], fs[1][1], fs[1][2], fs[1][3], fs[1][4], fs[1][5], fs[3][6], fs[3][7]],
+                    [fs[2][6], fs[2][7], fs[2][0], fs[2][1], fs[2][2], fs[2][3], fs[2][4], fs[2][5]],
+                    [fs[0][0], fs[3][1], fs[3][2], fs[3][3], fs[3][4], fs[3][5], fs[0][6], fs[0][7]],
+                    [fs[4][0], fs[4][1], fs[4][2], fs[4][3], fs[4][4], fs[4][5], fs[4][6], fs[4][7]],
+                    [fs[5][0], fs[5][1], fs[2][6], fs[2][7], fs[2][0], fs[5][5], fs[5][6], fs[5][7]],
+                ]
+            },
+            Move::Lp => for _ in 0..3 {
+                self.apply_move(&Move::L);
+            },
+            Move::L2 => for _ in 0..2 {
+                self.apply_move(&Move::L);
+            },
             Move::U => todo!(),
             Move::Up => todo!(),
+            Move::U2 => todo!(),
             Move::D => todo!(),
             Move::Dp => todo!(),
+            Move::D2 => todo!(),
             Move::F => todo!(),
             Move::Fp => todo!(),
+            Move::F2 => todo!(),
             Move::B => todo!(),
             Move::Bp => todo!(),
+            Move::B2 => todo!(),
         }
+    }
+    pub fn check_parity(&self) -> bool {
+        true
     }
 }
 
@@ -116,10 +160,10 @@ impl fmt::Display for Cube3by3 {
             fs[5][7], fs[5][3],
         );
         fmtd += &format!("{}{}{}{}{}{}{}{}{}{}{}{}\n",
-            fs[2][0], fs[2][1], fs[2][2],
-            fs[3][0], fs[3][1], fs[3][2],
-            fs[4][0], fs[4][1], fs[4][2],
-            fs[5][0], fs[5][1], fs[5][2],
+            fs[2][6], fs[2][5], fs[2][4],
+            fs[3][6], fs[3][5], fs[3][4],
+            fs[4][6], fs[4][5], fs[4][4],
+            fs[5][6], fs[5][5], fs[5][4],
         );
 
         fmtd += &format!("      {}{}{}\n", fs[1][0], fs[1][1], fs[1][2]);
@@ -133,20 +177,27 @@ impl fmt::Display for Cube3by3 {
 pub enum Move {
     R,
     Rp,
+    R2,
     L,
     Lp,
+    L2,
     U,
     Up,
+    U2,
     D,
     Dp,
+    D2,
     F,
     Fp,
+    F2,
     B,
     Bp,
+    B2,
 }
 
 #[cfg(test)]
 mod tests {
+    use super::Move::*;
     use super::*;
 
     fn align(repr: &str) -> String {
@@ -156,9 +207,8 @@ mod tests {
     #[test]
     fn print_solved() {
         let cube = Cube3by3::new();
-        let fmtd = format!("{}", cube);
         assert_eq!(
-            fmtd,
+            cube.to_string(),
             align(
                 "\
       ⬜⬜⬜
@@ -170,6 +220,30 @@ mod tests {
       🟨🟨🟨
       🟨🟨🟨
       🟨🟨🟨"
+            )
+        )
+    }
+
+    #[test]
+    fn scramble() {
+        let mut cube = Cube3by3::new();
+        cube.apply(&[
+            D2, R2, U2, L2, Dp, R2, Dp, B2, D2, F2, Dp, Fp, Rp, Fp, Dp, R, U2, Fp, Lp, Rp,
+        ]);
+        println!("{}", cube);
+        assert_eq!(
+            cube.to_string(),
+            align(
+                "\
+      🟩🟩🟦
+      🟦⬜🟩
+      ⬜🟧⬜
+🟨🟨🟧🟩🟨🟥🟦🟧🟧⬜🟨🟧
+🟥🟧🟩⬜🟩🟨🟥🟥🟧⬜🟦🟩
+🟩🟥🟦🟨🟦🟦🟧🟥⬜🟥🟦🟨
+      🟥🟧🟨
+      🟦🟨⬜
+      🟥⬜🟩"
             )
         )
     }
